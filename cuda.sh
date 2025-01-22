d01b30f2f9d51db096bd14bcd39f8464ca0982e1
marmik <103449011+MarmikChaudhari@users.noreply.github.com>
2025-01-22 03:00:50 -0500

    new bash

--- cuda.sh
+++ cuda.sh
@@ -1,7 +1,11 @@
 # Update the system and install necessary packages
 echo "Updating system packages..."
 apt update && apt upgrade -y
-apt install cuda-toolkit-12-1
+apt install wget -y
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.0-1_all.deb
+dpkg -i cuda-keyring_1.0-1_all.deb
+apt update
+apt install cuda-toolkit-12-1 -y
 
 # Install PyTorch with specific index URL
 echo "Installing PyTorch..."
@@ -9,7 +13,7 @@ pip3 install torch==2.4.1 torchvision torchaudio --index-url https://download.py
 
 # Install other Python dependencies
 echo "Installing Python dependencies..."
-pip3 install kaleido transformers==4.44.2 tqdm pandas numpy
+pip3 install kaleido transformers==4.44.2 tqdm pandas numpy accelerate
 
 # set cuda environment variables
 export CUDA_HOME=/usr/local/cuda
@@ -20,6 +24,6 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_HOME/lib64
 pip3 install flash-attn --no-build-isolation
 
 # install plotly matplotlib
-pip3 install plotly matplotlib
+pip3 install plotly matplotlib scikit-learn
 
 echo "All dependencies installed successfully!"
